bc2b0d089da05d63b68f0567f4801a969dbd40a9
jasoncUD <jasonc@udel.edu>
2023-11-20 00:27:26 -0500

    First Commit Message

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and Make a Workout Planner!
         </a>
       </header>
     </div>
