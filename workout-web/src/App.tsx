3d28dd46816ca52d7978cf59dfd5cf87d7ba1bcf
jasoncUD <jasonc@udel.edu>
2023-11-22 12:48:00 -0500

    Added bootstrap

--- workout-web/src/App.tsx
+++ workout-web/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WorkoutPlan from './components/WorkoutPlan/WorkoutPlan';
+import { Button } from "react-bootstrap";
 
 function App() {
+  const [isWorkoutPlan, setIsWorkoutPlan] = useState<boolean>(false);
   return (
     <div className="App">
-      Workout Planner!
+      Welcome to the Workout Planner!
+      <Button></Button>
       <WorkoutPlan></WorkoutPlan>
     </div>
   );
