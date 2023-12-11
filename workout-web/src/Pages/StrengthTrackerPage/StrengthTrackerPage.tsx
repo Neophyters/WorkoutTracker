47539832d4d13c3eae28f7e5c7ab56c04f976480
jasoncUD <jasonc@udel.edu>
2023-12-11 16:00:55 -0500

    Fixed warnings

--- workout-web/src/Pages/StrengthTrackerPage/StrengthTrackerPage.tsx
+++ workout-web/src/Pages/StrengthTrackerPage/StrengthTrackerPage.tsx
@@ -1,14 +1,17 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 interface StrengthTrackerPageProps {}
 
 const StrengthTrackerPage: FC<StrengthTrackerPageProps> = () => {
-   return (
-   <div>
-            <h1 style={{ textAlign: 'center' }}>Track Your Strength!!</h1>
 
-   </div>
-   );
+  return (
+
+    <div>
+      <h1 style={{ textAlign: "center" }}>Track Your Strength!!</h1>
+      <p style={{ textAlign: "center" }}>This is the Strength Tracker Page</p>
+
+    </div>
+  );
 };
 
 export default StrengthTrackerPage;
