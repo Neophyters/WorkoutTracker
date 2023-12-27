239f80177e72fa594e16e06d3613c3e47745fdc7
jasoncUD <jasonc@udel.edu>
2023-12-27 01:22:27 -0500

    New Checkpoint with ideas for the HomePage

--- workout-web/src/Pages/HomePage/HomePage.tsx
+++ workout-web/src/Pages/HomePage/HomePage.tsx
@@ -1,13 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 interface HomePageProps {}
 
 const HomePage: FC<HomePageProps> = () => {
-   return (
-   <div>
-        <h1 style={{ textAlign: 'center' }}>Hello this is the Home Page!!</h1>
-   </div>
-   );
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Hello this is the Home Page!!</h1>
+      <ul>
+         <li>Allow the User to search for a workout.</li>
+         <li>Allow to get inspired and follow workout plans.</li>
+         <li>Can have them view push, pull, legs, full body, calisthenic, athletic workouts, etc.</li>
+         <li>In later features have a leaderboard for all users to compete and get motivated to be better.</li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
