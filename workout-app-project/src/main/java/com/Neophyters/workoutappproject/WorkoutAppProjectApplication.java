1f9b02b36a7bbabc397666d1c4ba546c63800a0f
jasoncUD <jasonc@udel.edu>
2024-01-06 01:19:56 -0500

    Made .env file to hide passwords

--- workout-app-project/src/main/java/com/Neophyters/workoutappproject/WorkoutAppProjectApplication.java
+++ workout-app-project/src/main/java/com/Neophyters/workoutappproject/WorkoutAppProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WorkoutAppProjectApplication {
 
 	public static void main(String[] args) {
+		DotenvLoader.loadDotenv();
 		SpringApplication.run(WorkoutAppProjectApplication.class, args);
 	}
 
