8b88bd4e9a00188c3917130f892cbf32ba89318d
jasoncUD <jasonc@udel.edu>
2024-01-06 01:23:16 -0500

    Dotenv class added

---
+++ workout-app-project/src/main/java/com/Neophyters/workoutappproject/DotenvLoader.java
@@ -0,0 +1,26 @@
+package com.Neophyters.workoutappproject;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DotenvLoader {
+
+    public static void loadDotenv() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(".env"));
+            for (String line : lines) {
+                String[] parts = line.split("=", 2);
+                if (parts.length == 2) {
+                    String key = parts[0];
+                    String value = parts[1];
+                    System.setProperty(key, value);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
